d98796d3fdec3e3914c36677772403e9944e4c87
Rufus-tg <2738992140@qq.com>
2022-08-28 10:20:04 +0800

    version5.5

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ Vue.use(Router)
 export default new Router({
     routes: [
         {
-            path: '/',
-            redirect: '/login',
+            path: 'project-v2/',
+            redirect: 'project-v2/',
             name: '首页',
             hidden: true,
             component: () => import('@/components/Login')
         },
         {
-            path: '/login',
+            path: 'project-v2/',
             name: 'Login',
             hidden: true,
             component: () => import('@/components/Login')
@@ -32,38 +32,38 @@ export default new Router({
         //     component: resolve => require(['@/components/Home'], resolve)
         // },
         {
-            path: '/home',
+            path: 'project-v2/home',
             name: '学生管理',
             iconClass: 'fa fa-users',
             redirect: '/home/student',
             component: () => import('@/components/Home'),
             children: [
                 {
-                    path: '/home/student',
+                    path: 'project-v2/home/student',
                     name: '学生列表',
                     iconClass: 'fa fa-list',
                     component: () => import('@/components/students/StudentList')
                 },
                 {
-                    path: '/home/info',
+                    path: 'project-v2/home/info',
                     name: '信息列表',
                     iconClass: 'fa fa-list-alt',
                     component: () => import('@/components/students/InfoList')
                 },
                 {
-                    path: '/home/infos',
+                    path: 'project-v2/home/infos',
                     name: '信息管理',
                     iconClass: 'fa fa-list-alt',
                     component: () => import('@/components/students/InfoLists')
                 },
                 {
-                    path: '/home/work',
+                    path: 'project-v2/home/work',
                     name: '作业列表',
                     iconClass: 'fa fa-list-ul',
                     component: () => import('@/components/students/WorkList')
                 },
                 {
-                    path: '/home/works',
+                    path: 'project-v2/home/works',
                     name: '作业管理',
                     iconClass: 'fa fa-th-list',
                     component: () => import('@/components/students/WorkMent')
@@ -71,31 +71,31 @@ export default new Router({
             ]
         },
         {
-            path: '/home',
+            path: 'project-v2/home',
             name: '数据分析',
             iconClass: 'fa fa-bar-chart',
             component: () => import('@/components/Home'),
             children: [
                 {
-                    path: '/home/dataview',
+                    path: 'project-v2/home/dataview',
                     name: '数据概览',
                     iconClass: 'fa fa-line-chart',
                     component: () => import('@/components/dataAnalysis/DataView')
                 },
                 {
-                    path: '/home/mapview',
+                    path: 'project-v2/home/mapview',
                     name: '地图概览',
                     iconClass: 'fa fa-line-chart',
                     component: () => import('@/components/dataAnalysis/MapView')
                 },
                 {
-                    path: '/home/travel',
+                    path: 'project-v2/home/travel',
                     name: '旅游地图',
                     iconClass: 'fa fa-line-chart',
                     component: () => import('@/components/dataAnalysis/TravelMap')
                 },
                 {
-                    path: '/home/score',
+                    path: 'project-v2/home/score',
                     name: '分数地图',
                     iconClass: 'fa fa-line-chart',
                     component: () => import('@/components/dataAnalysis/ScoreMap')
@@ -103,13 +103,13 @@ export default new Router({
             ]
         },
         {
-            path: '/users',
+            path: 'project-v2/users',
             name: '用户中心',
             iconClass: 'fa fa-user',
             component: () => import('@/components/Home'),
             children: [
                 {
-                    path: '/users/user',
+                    path: 'project-v2/users/user',
                     name: '权限管理',
                     iconClass: 'fa fa-user',
                     component: () => import('@/components/users/User')
